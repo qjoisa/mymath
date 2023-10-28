7fd8b819f690d6c7dfa89c21e151358a8bf4a729
QJoisa <ujmin04@gmail.com>
2023-10-28 17:40:54 +0500

    math

---
+++ math.go
@@ -0,0 +1,27 @@
+package mymath
+
+import "math"
+
+func Sqrt(a float64) float64 {
+	return math.Sqrt(a)
+}
+
+func Ceil(a float64) float64 {
+	return math.Ceil(a)
+}
+
+func Floor(a float64) float64 {
+	return math.Floor(a)
+}
+
+func Pow(a, b float64) float64 {
+	return math.Pow(a, b)
+}
+
+func Max(a, b float64) float64 {
+	return math.Max(a, b)
+}
+
+func Min(a, b float64) float64 {
+	return math.Min(a, b)
+}
